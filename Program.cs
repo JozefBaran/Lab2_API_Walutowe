ed2d51f2fd2d1498bd1d0f578ad3ff8506e00c2c
JozefBaran <103137803+JozefBaran@users.noreply.github.com>
2024-04-03 12:57:27 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,17 +1,46 @@
-namespace GuiWalutowe
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.Json;
+using ApiWalutowe;
+
+class Program
 {
-    internal static class Program
+    static async Task Main(string[] args)
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        Console.WriteLine("Podaj date w formacie RRRR-MM-DD");
+        string? data = Console.ReadLine();
+
+        string format = "rrrr-MM-dd";
+
+        bool isDateValid = DateTime.TryParseExact(data, format, null, System.Globalization.DateTimeStyles.None, out _);
+        if (isDateValid)
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Console.WriteLine("Valid data. Try again.");
+        }
+        else
+        {
+            Console.WriteLine("Data została podana w prawidłowym formacie.");
+        }
+
+        var appId = "17c45a9d1b0a4d34acda9363b66acaae";
+        var url = $"https://openexchangerates.org/api/historical/{data}.json?app_id={appId}";
+
+
+        using (var Client = new HttpClient())
+        {
+            try
+            {
+                var response = await Client.GetStringAsync(url);
+                Console.WriteLine(response);
+                var exchangeRates = JsonSerializer.Deserialize<ExchangeRatesResponse>(response);
+                exchangeRates.Date = data;
+                Console.WriteLine(exchangeRates.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
         }
     }
 }
