8ff41ac1c3e5d0c5f0ca863ed93e60cf89523462
jozef <jozef@DESKTOP-N1MGSAJ>
2024-04-03 12:54:19 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.Json;
+using ApiWalutowe;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Podaj date w formacie RRRR-MM-DD");
+        string? data = Console.ReadLine();
+
+        string format = "rrrr-MM-dd";
+
+        bool isDateValid = DateTime.TryParseExact(data, format, null, System.Globalization.DateTimeStyles.None, out _);
+        if (isDateValid)
+        {
+            Console.WriteLine("Valid data. Try again.");
+        }
+        else
+        {
+            Console.WriteLine("Data została podana w prawidłowym formacie.");
+        }
+
+        var appId = "17c45a9d1b0a4d34acda9363b66acaae";
+        var url = $"https://openexchangerates.org/api/historical/{data}.json?app_id={appId}";
+
+
+        using (var Client = new HttpClient())
+        {
+            try
+            {
+                var response = await Client.GetStringAsync(url);
+                Console.WriteLine(response);
+                var exchangeRates = JsonSerializer.Deserialize<ExchangeRatesResponse>(response);
+                exchangeRates.Date = data;
+                Console.WriteLine(exchangeRates.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Waluta.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace ApiWalutowe
+{
+    public class ExchangeRatesResponse
+    {
+       [JsonPropertyName("rates")]
+        public Dictionary<string, double> Rates { get; set; }
+
+       [JsonPropertyName("base")]
+        public string? Base { get; set; }
+        public string? Date { get; set; }
+
+        public ExchangeRatesResponse()
+        {
+            Rates = new Dictionary<string, double>();
+        }
+        public override string ToString()
+        {   
+            double searchingRate = 0;
+            if (Rates.TryGetValue("PLN", out searchingRate))
+            {
+                return $"Dnia {Date} 1 {Base} kosztował {searchingRate} PLN.";
+            }
+            else
+            {
+                return $"Dnia {Date} kurs PLN nie jest dostępny dla {Base}.";
+            }
+        }
+    }
+}
